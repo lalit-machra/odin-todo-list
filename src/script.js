56a65c0ef61dff04b51796c74c9c8e503e8dde7f
lalit-machra <lalitmachra0403@gmail.com>
2024-12-13 14:35:30 +0530

    Add classes for generating todo items and projects

--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
 import "./style.css"
 
-console.log("hii");
+class projectGenerator {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class todoItemGenerator {
+  constructor(description, category, dueTime, dueDate, priority, project) {
+    this.description = description;
+    this.category = category
+    this.dueTime = dueTime;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.status = "not-completed";
+    this.project = project;
+  }
+}
+
+const todo2 = new todoItemGenerator("Wash clothes", "Home","10:00AM", "25/10/2024", "2");
+console.log(todo2);
